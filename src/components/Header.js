9ab40593c1a0d6176c582785c110062e6cf2f03a
teja679 <lovlytejaa@gmail.com>
2022-11-06 19:33:08 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <h1>This is the Header</h1>
+
+        <div className='header_left'>
+
+        </div>
+        
+        <div className='header_left'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
